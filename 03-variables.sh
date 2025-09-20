c0e8dd6368368b7084909a5b1c11e495e6e3c239
Akhila <akhichowdary00@gmail.com>
2025-09-20 22:05:16 +0530

    some message

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+# no space betwen variable and value in the below 4 , 5 lines
+ person1=Ramesh
+ person2=suresh
+
+echo "$person1:: Hi $person2"
+echo "$person2:: Hello $person1"
+echo "$peron1:: where are you"
+echo "$person2:: I'm In banglore"
+
