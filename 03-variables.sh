a7539223edc3dc79c1d35a342cab528ee63cb24a
Akhila <akhichowdary00@gmail.com>
2025-09-20 22:06:25 +0530

    some message

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,8 +1,8 @@
 #!bin/bash
 
 # no space betwen variable and value in the below 4 , 5 lines
- person1=Ramesh
- person2=suresh
+ person1=Akhila
+ person2=Ramya
 
 echo "$person1:: Hi $person2"
 echo "$person2:: Hello $person1"
