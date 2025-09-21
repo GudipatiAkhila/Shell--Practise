43fa98f18d2a0494357ab81f399e814cf38d95c0
Akhila <akhichowdary00@gmail.com>
2025-09-21 12:48:24 +0530

    soome message

--- 04-variables.sh
+++ 04-variables.sh
@@ -4,6 +4,6 @@ person1=$1
 person2=$2
 
 echo "$person1::welcome to india"
-echo "$person2:: Thank you $person1:
+echo "$person2:: Thank you "
 echo "$person1:: do we have adeal"
 echo "$person2:: yes if uou make ukraine"
