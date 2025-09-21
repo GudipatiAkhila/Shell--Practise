8339766090380c7b9b422bb9d0e80417ee670ec2
Akhila <akhichowdary00@gmail.com>
2025-09-21 21:43:10 +0530

    some message

--- 08-spc-variable.sh
+++ 08-spc-variable.sh
@@ -5,4 +5,8 @@ echo "All variables passed to the script: $*"
 echo "script name: $0"
 echo "current directory:$pwd"
 echo "who is running this: $USER"
+echo "Home Directory of user: $Home'
+echo :PID of this Script: $$"
+sleep 50 & 
+echo "PID of the last command in background is: $!"
 
