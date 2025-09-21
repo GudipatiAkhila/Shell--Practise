fe2e402a31119970057c9ec65a1349024a888d20
Akhila <akhichowdary00@gmail.com>
2025-09-21 21:36:06 +0530

    some message

--- 08-spc-variable.sh
+++ 08-spc-variable.sh
@@ -2,5 +2,7 @@
 
 echo "All variables passed to the script: $@"
 echo "All variables passed to the script: $*"
-
+echo "script name: $0"
+echo "current directory:$pwd"
+echo "who is running this: $USER"
 
