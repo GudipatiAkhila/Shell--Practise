2dd86ccdc33756a32c02096b28bcbd266283a30d
Akhila <akhichowdary00@gmail.com>
2025-09-21 22:13:05 +0530

    some message

--- 09-data-types.sh
+++ 09-data-types.sh
@@ -4,7 +4,11 @@
 #everything in shell as consider as string
 NUMBER1=100
 NUMBER2=200
+Name=Devops
 
 sum=$(($NUMBER1+$NUMBER2))
 
 echo "SUM is: ${sum}"
+
+LEADERS={"AKHI","AKHILA"}
+echo "All leaders: ${LEADERS}[@]"
