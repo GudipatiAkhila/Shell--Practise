d1b4d3ea46c52343813068fc80a44f1dfff2e02f
Akhila <akhichowdary00@gmail.com>
2025-09-30 22:46:48 +0530

    some message

--- 18-loops.sh
+++ 18-loops.sh
@@ -8,38 +8,37 @@ N="\e[0m"
 
 LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.LOG"
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 
-mkdir -p $LOGS_FOLDER 
-echo "script started executed at: $(date)" | tee -a $LOG_FILE
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
 if [ $USERID -ne 0 ]; then
-    echo "ERROR:: please run this script with root privelege"
-    exit 1
-fi 
-VALIDATE(){
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
+fi
+
+VALIDATE(){ # functions receive inputs through args just like shell script args
     if [ $1 -ne 0 ]; then
-        echo -e "Installing $2 ... $R FAILURE $N" | tee -a $LOG_FILE 
+        echo -e "Installing $2 ... $R FAILURE $N" | tee -a $LOG_FILE
         exit 1
     else
         echo -e "Installing $2 ... $G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
 
-
 # $@
 
 for package in $@
 do
-    #check package is alredy installed or not
-    dnf list installed $package &??$LOG_FILE
+    # check package is already installed or not
+    dnf list installed $package &>>$LOG_FILE
 
-    #if exit status is 0 , alredy installed , -ne 0 need to install it 
-    if [ $? -ne 0 ]; then 
+    # if exit status is 0, already installed. -ne 0 need to install it
+    if [ $? -ne 0 ]; then
         dnf install $package -y &>>$LOG_FILE
         VALIDATE $? "$package"
-
     else
-        echo -e "$package alredy installed ... $Y SKIPPING $N" 
+        echo -e "$package already installed ... $Y SKIPPING $N"
     fi
 done
